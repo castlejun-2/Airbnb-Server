a89529c1ff165543317f125a29c40b0760e636ea
이성준 <75744297+castlejun-2@users.noreply.github.com>
2021-08-03 03:02:33 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: 'database-1.cndywyl1g5hh.ap-northeast-2.rds.amazonaws.com', //RDS 서버 End point 설정
-    user: 'millidb', //user의 계정명
+    host: '', //RDS 서버 End point 설정
+    user: '', //user의 계정명
     port: '3306',
-    password: 'vibccku2!', //고유 password
+    password: '', //고유 password
     database: 'milli' //database schema
 });
 
